5733d91ff6c05490450a1e23cc57fbf91b2438f6
ACER <loilyxtra@gmail.com>
2022-05-09 00:43:52 +0700

    finished: main navigation that contains home, forum, leaderboard, and profile

--- app/src/main/java/com/ramiyon/soulmath/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/ramiyon/soulmath/presentation/ui/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.ramiyon.soulmath.presentation.ui
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.ramiyon.soulmath.R
+import com.ramiyon.soulmath.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            this.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            window.statusBarColor = resources.getColor(R.color.white)
+        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setUpNavigation()
+    }
+
+    private fun setUpNavigation() {
+        val navController = findNavController(R.id.main_navigation)
+        binding.mainBottomNav.setupWithNavController(navController)
     }
 }
