004aecf52b0f7bd9ea1991fedc4e3e4dcd1014ef
ACER <loilyxtra@gmail.com>
2022-05-30 01:03:59 +0700

    finished: Add page transformation for the view pager

--- app/src/main/java/com/ramiyon/soulmath/presentation/diff_callback/BaseDiffUtil.kt
+++ app/src/main/java/com/ramiyon/soulmath/presentation/diff_callback/BaseDiffUtil.kt
@@ -2,14 +2,12 @@ package com.ramiyon.soulmath.presentation.diff_callback
 
 import androidx.recyclerview.widget.DiffUtil
 
-abstract class BaseDiffUtil<ListType, ItemIdentifier, ContentIdentifier>(
+abstract class BaseDiffUtil<ListType, ItemIdentifier>(
     private val oldList: List<ListType>,
     private val newList: List<ListType>
 ): DiffUtil.Callback() {
 
     abstract fun ListType.getItemIdentifier(): ItemIdentifier
-
-    abstract fun ListType.getContentIdentifier(): ContentIdentifier
 
     override fun getOldListSize(): Int = oldList.size
 
@@ -19,5 +17,5 @@ abstract class BaseDiffUtil<ListType, ItemIdentifier, ContentIdentifier>(
         oldList[oldItemPosition].getItemIdentifier() == newList[newItemPosition].getItemIdentifier()
 
     override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
-        oldList[oldItemPosition].getContentIdentifier() == newList[newItemPosition].getContentIdentifier()
+        oldList[oldItemPosition] == newList[newItemPosition]
 }
--- app/src/main/java/com/ramiyon/soulmath/presentation/diff_callback/LastMaterialDiffCallback.kt
+++ app/src/main/java/com/ramiyon/soulmath/presentation/diff_callback/LastMaterialDiffCallback.kt
@@ -3,13 +3,9 @@ package com.ramiyon.soulmath.presentation.diff_callback
 class LastMaterialDiffCallback(
     oldList: List<String>,
     newList: List<String>
-): BaseDiffUtil<String, Int, Int>(oldList, newList) {
+): BaseDiffUtil<String, Int>(oldList, newList) {
 
     override fun String.getItemIdentifier(): Int {
-        return this.toInt()
-    }
-
-    override fun String.getContentIdentifier(): Int {
         return this.toInt()
     }
 }
