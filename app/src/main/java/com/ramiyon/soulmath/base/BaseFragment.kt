7065c3e37886307ded3c5e2fa98b0041466c8386
ACER <loilyxtra@gmail.com>
2022-06-26 19:24:08 +0700

    on progress: add BaseFragment.kt abstract class to handle base class of Fragment

---
+++ app/src/main/java/com/ramiyon/soulmath/base/BaseFragment.kt
@@ -0,0 +1,45 @@
+package com.ramiyon.soulmath.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.viewbinding.ViewBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+abstract class BaseFragment<VB: ViewBinding, VM: ViewModel>: Fragment() {
+
+    val viewModel by viewModel<ViewModel>()
+    private var _binding: VB? = null
+    private val binding get() = _binding
+
+    abstract fun inflateViewBinding(container: ViewGroup?): VB
+    abstract fun VB.binder(): () -> Unit
+
+    fun onCreateViewBehaviour(): () -> Unit = {}
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = inflateViewBinding(container)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.apply {
+            binder()
+        }
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+}
--- app/src/main/java/com/ramiyon/soulmath/presentation/ui/leaderboard/LeaderboardFragment.kt
+++ app/src/main/java/com/ramiyon/soulmath/presentation/ui/leaderboard/LeaderboardFragment.kt
@@ -1,38 +1,20 @@
 package com.ramiyon.soulmath.presentation.ui.leaderboard
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import com.ramiyon.soulmath.R
+import com.ramiyon.soulmath.base.BaseFragment
 import com.ramiyon.soulmath.databinding.FragmentLeaderboardBinding
 import com.ramiyon.soulmath.presentation.adapter.LeaderboardAdapter
 import org.koin.android.ext.android.inject
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class LeaderboardFragment : Fragment() {
+class LeaderboardFragment : BaseFragment<FragmentLeaderboardBinding, LeaderboardViewModel>() {
 
-    private val viewModel by viewModel<LeaderboardViewModel>()
-    private var _binding: FragmentLeaderboardBinding? = null
-    private val binding get() = _binding
     private val adapter by inject<LeaderboardAdapter>()
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentLeaderboardBinding.inflate(layoutInflater, container, false)
-        return binding?.root
-    }
+    override fun inflateViewBinding(container: ViewGroup?): FragmentLeaderboardBinding =
+        FragmentLeaderboardBinding.inflate(layoutInflater, container, false)
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
-
-    override fun onDestroyView() {
-        _binding = null
-        super.onDestroyView()
+    override fun FragmentLeaderboardBinding.binder(): () -> Unit {
+        TODO("Not yet implemented")
     }
 
 }
