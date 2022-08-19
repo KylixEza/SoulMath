cbc3d92b9b4e64f553bf56bef617e1bf165a99eb
ACER <loilyxtra@gmail.com>
2022-08-19 11:01:34 +0700

    update: rename MaterialOnBoardImpl.kt to MaterialOnBoardUseCaseImpl.kt

--- app/src/main/java/com/ramiyon/soulmath/domain/usecase/material/onboard/MaterialOnBoardUseCaseImpl.kt
+++ app/src/main/java/com/ramiyon/soulmath/domain/usecase/material/onboard/MaterialOnBoardUseCaseImpl.kt
@@ -2,7 +2,7 @@ package com.ramiyon.soulmath.domain.usecase.material.onboard
 
 import com.ramiyon.soulmath.data.source.dummy.MaterialOnBoardContent
 
-class MaterialOnBoardImpl: MaterialOnBoardUseCase {
+class MaterialOnBoardUseCaseImpl: MaterialOnBoardUseCase {
     override fun getMaterialOnBoardContentById(materialId: String, page: Int) = MaterialOnBoardContent.getMaterialOnBoardContentById(materialId, page)
     override fun getMaterialLearningPurposeById(materialId: String) = MaterialOnBoardContent.getMaterialLearningPurposeById(materialId)
 }
