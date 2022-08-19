f11b86ab3578c9450b5662fd2824c97886ba0f98
ACER <loilyxtra@gmail.com>
2022-08-19 10:53:15 +0700

    update: paired view pager on material and the view pager adapter

--- app/src/main/java/com/ramiyon/soulmath/presentation/ui/material/onboard/MaterialOnBoardActivity.kt
+++ app/src/main/java/com/ramiyon/soulmath/presentation/ui/material/onboard/MaterialOnBoardActivity.kt
@@ -2,6 +2,10 @@ package com.ramiyon.soulmath.presentation.ui.material.onboard
 
 import com.ramiyon.soulmath.base.BaseActivity
 import com.ramiyon.soulmath.databinding.ActivityMaterialOnBoardBinding
+import com.ramiyon.soulmath.presentation.adapter.OnBoardingViewPagerAdapter
+import com.ramiyon.soulmath.presentation.ui.material.onboard.screens.first.MaterialOnBoardFirstScreenFragment
+import com.ramiyon.soulmath.presentation.ui.material.onboard.screens.second.MaterialOnBoardSecondScreenFragment
+import com.ramiyon.soulmath.presentation.ui.material.onboard.screens.third.MaterialOnBoardThirdScreenFragment
 import com.ramiyon.soulmath.util.ScreenOrientation
 
 class MaterialOnBoardActivity : BaseActivity<ActivityMaterialOnBoardBinding>() {
@@ -9,12 +13,25 @@ class MaterialOnBoardActivity : BaseActivity<ActivityMaterialOnBoardBinding>() {
         return ActivityMaterialOnBoardBinding.inflate(layoutInflater)
     }
 
-    override fun determineScreenOrientation(): ScreenOrientation? {
-        return ScreenOrientation.PORTRAIT
-    }
+    override fun determineScreenOrientation(): ScreenOrientation? = null
 
-    override fun ActivityMaterialOnBoardBinding?.binder() {
+    override fun ActivityMaterialOnBoardBinding.binder() {
+        val adapter = OnBoardingViewPagerAdapter(
+            supportFragmentManager,
+            lifecycle
+        )
 
+        val fragments = listOf(
+            MaterialOnBoardFirstScreenFragment(),
+            MaterialOnBoardSecondScreenFragment(),
+            MaterialOnBoardThirdScreenFragment()
+        )
+
+        adapter.apply {
+            setAllFragments(fragments)
+            vpMaterialOnboard.adapter = this
+        }
+        pageIndicatorViewMaterialOnboard.setViewPager2(vpMaterialOnboard)
     }
 
 }
