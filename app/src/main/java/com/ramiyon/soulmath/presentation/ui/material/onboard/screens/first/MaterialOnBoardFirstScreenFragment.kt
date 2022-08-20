0223c1c80150edcb140cfd24fdf377ff4bcabc44
ACER <loilyxtra@gmail.com>
2022-08-20 22:04:22 +0700

    update: paired data to each views

--- app/src/main/java/com/ramiyon/soulmath/presentation/ui/material/onboard/screens/first/MaterialOnBoardFirstScreenFragment.kt
+++ app/src/main/java/com/ramiyon/soulmath/presentation/ui/material/onboard/screens/first/MaterialOnBoardFirstScreenFragment.kt
@@ -1,9 +1,11 @@
 package com.ramiyon.soulmath.presentation.ui.material.onboard.screens.first
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.bumptech.glide.Glide
 import com.ramiyon.soulmath.base.BaseFragment
+import com.ramiyon.soulmath.databinding.ActivityMaterialOnBoardBinding
 import com.ramiyon.soulmath.databinding.FragmentMaterialOnBoardFirstScreenBinding
 import com.ramiyon.soulmath.util.Constanta.ARG_MODULE_ID
 import com.ramiyon.soulmath.util.ScreenOrientation
@@ -12,6 +14,7 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 class MaterialOnBoardFirstScreenFragment : BaseFragment<FragmentMaterialOnBoardFirstScreenBinding>() {
 
     private val viewModel by viewModel<MaterialOnBoardFirstScreenViewModel>()
+    private lateinit var parentBinding: ActivityMaterialOnBoardBinding
     private var materialId: String? = null
 
     companion object {
@@ -29,6 +32,10 @@ class MaterialOnBoardFirstScreenFragment : BaseFragment<FragmentMaterialOnBoardF
         }
     }
 
+    override fun onCreateViewBehaviour(inflater: LayoutInflater, container: ViewGroup?) {
+        parentBinding = ActivityMaterialOnBoardBinding.inflate(inflater, container, false)
+    }
+
     override fun inflateViewBinding(container: ViewGroup?): FragmentMaterialOnBoardFirstScreenBinding {
         return FragmentMaterialOnBoardFirstScreenBinding.inflate(layoutInflater, container, false)
     }
@@ -40,6 +47,10 @@ class MaterialOnBoardFirstScreenFragment : BaseFragment<FragmentMaterialOnBoardF
             .load(onBoardContent.gif)
             .into(ivGifMaterialFirstOnboard)
         tvMaterialFirstOnboard.text = onBoardContent.description
+
+        btnNextMaterialFirstOnboard.setOnClickListener {
+            parentBinding.vpMaterialOnboard.currentItem = 1
+        }
     }
 
     override fun determineScreenOrientation(): ScreenOrientation {
