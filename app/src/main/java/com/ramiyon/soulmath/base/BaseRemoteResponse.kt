446ba18cf72244df4a58e471138c1cb38bb5916e
ACER <loilyxtra@gmail.com>
2022-08-27 22:12:36 +0700

    fix: error while fetch data because of flow catch

--- app/src/main/java/com/ramiyon/soulmath/base/BaseRemoteResponse.kt
+++ app/src/main/java/com/ramiyon/soulmath/base/BaseRemoteResponse.kt
@@ -4,31 +4,28 @@ import android.util.Log
 import com.ramiyon.soulmath.data.source.remote.api.response.BaseResponse
 import com.ramiyon.soulmath.data.util.RemoteResponse
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 
 abstract class BaseRemoteResponse<RequestType> {
 
     private val value = flow {
-        try {
-            val response = call()
-            val data = response.data
-            Log.d("Base Remote Response", response.data.toString())
-            if (response.status == "200") {
-                if(data is List<*>) {
-                    if (response.count == 0)
-                        emit(RemoteResponse.Empty())
-                    else
-                        emit(RemoteResponse.Success(data))
-                } else {
+        val response = call()
+        val data = response.data
+        Log.d("Base Remote Response", response.data.toString())
+        if (response.status == "200") {
+            if(data is List<*>) {
+                if (response.count == 0)
+                    emit(RemoteResponse.Empty())
+                else
                     emit(RemoteResponse.Success(data))
-                }
             } else {
-                throw Exception(response.message)
+                emit(RemoteResponse.Success(data))
             }
-        } catch (e: Exception) {
-            emit(RemoteResponse.Error(e.message.toString()))
         }
+    }.catch {
+        emit(RemoteResponse.Error(it.message.toString()))
     }.flowOn(Dispatchers.IO)
 
     abstract suspend fun call(): BaseResponse<RequestType>
