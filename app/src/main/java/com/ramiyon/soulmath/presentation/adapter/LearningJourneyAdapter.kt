751423add732f1ffc2d04a17c53be40d794fb8b7
ACER <loilyxtra@gmail.com>
2022-09-03 21:31:01 +0700

    update: simplified the code (remove the boilerplate code)

--- app/src/main/java/com/ramiyon/soulmath/presentation/adapter/LearningJourneyAdapter.kt
+++ app/src/main/java/com/ramiyon/soulmath/presentation/adapter/LearningJourneyAdapter.kt
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.DiffUtil
 import com.bumptech.glide.Glide
 import com.ramiyon.soulmath.R
 import com.ramiyon.soulmath.base.BaseRecyclerViewAdapter
+import com.ramiyon.soulmath.databinding.IncludeSubModuleProgressBarBinding
 import com.ramiyon.soulmath.databinding.ItemListLearningJourneyBinding
 import com.ramiyon.soulmath.domain.model.learning_journey.LearningJourney
 import com.ramiyon.soulmath.presentation.diff_callback.LearningJourneyDiffUtil
@@ -26,71 +27,43 @@ class LearningJourneyAdapter: BaseRecyclerViewAdapter<ItemListLearningJourneyBin
 
             pbMaterial.progress = item.materialLearningJourneyResponse.materialPercentage.toInt()
 
-
-
             firstSubModule.apply {
-                Glide.with(itemView!!.context).load(item.moduleIconLocked).into(ivSubModuleIcon)
-                val firstSubModule = item.gameLearningJourneyResponses[0]
-                apbSubModule.progress = firstSubModule.gamePercentage.toInt()
-                if(firstSubModule.isDifficultiesCrowned.easy)
-                    Glide.with(itemView!!.context).load(R.drawable.ic_crowned).into(ivSubModuleFirstCrown)
-                else
-                    Glide.with(itemView!!.context).load(R.drawable.ic_uncrowned).into(ivSubModuleFirstCrown)
-
-                if(firstSubModule.isDifficultiesCrowned.medium)
-                    Glide.with(itemView!!.context).load(R.drawable.ic_crowned).into(ivSubModuleSecondCrown)
-                else
-                    Glide.with(itemView!!.context).load(R.drawable.ic_uncrowned).into(ivSubModuleSecondCrown)
-
-                if(firstSubModule.isDifficultiesCrowned.hard)
-                    Glide.with(itemView!!.context).load(R.drawable.ic_crowned).into(ivSubModuleThirdCrown)
-                else
-                    Glide.with(itemView!!.context).load(R.drawable.ic_uncrowned).into(ivSubModuleThirdCrown)
+                buildSubModuleProgressBar(item, 0)
             }
 
             secondSubModule.apply {
-                Glide.with(itemView!!.context).load(item.moduleIconLocked).into(ivSubModuleIcon)
-                val secondSubModule = item.gameLearningJourneyResponses[1]
-                apbSubModule.progress = secondSubModule.gamePercentage.toInt()
-                if(secondSubModule.isDifficultiesCrowned.easy)
-                    Glide.with(itemView!!.context).load(R.drawable.ic_crowned).into(ivSubModuleFirstCrown)
-                else
-                    Glide.with(itemView!!.context).load(R.drawable.ic_uncrowned).into(ivSubModuleFirstCrown)
-
-                if(secondSubModule.isDifficultiesCrowned.medium)
-                    Glide.with(itemView!!.context).load(R.drawable.ic_crowned).into(ivSubModuleSecondCrown)
-                else
-                    Glide.with(itemView!!.context).load(R.drawable.ic_uncrowned).into(ivSubModuleSecondCrown)
-
-                if(secondSubModule.isDifficultiesCrowned.hard)
-                    Glide.with(itemView!!.context).load(R.drawable.ic_crowned).into(ivSubModuleThirdCrown)
-                else
-                    Glide.with(itemView!!.context).load(R.drawable.ic_uncrowned).into(ivSubModuleThirdCrown)
+               buildSubModuleProgressBar(item, 1)
             }
 
             thirdSubModule.apply {
-                Glide.with(itemView!!.context).load(item.moduleIconLocked).into(ivSubModuleIcon)
-                val thirdSubModule = item.gameLearningJourneyResponses[2]
-                apbSubModule.progress = thirdSubModule.gamePercentage.toInt()
-                if(thirdSubModule.isDifficultiesCrowned.easy)
-                    Glide.with(itemView!!.context).load(R.drawable.ic_crowned).into(ivSubModuleFirstCrown)
-                else
-                    Glide.with(itemView!!.context).load(R.drawable.ic_uncrowned).into(ivSubModuleFirstCrown)
-
-                if(thirdSubModule.isDifficultiesCrowned.medium)
-                    Glide.with(itemView!!.context).load(R.drawable.ic_crowned).into(ivSubModuleSecondCrown)
-                else
-                    Glide.with(itemView!!.context).load(R.drawable.ic_uncrowned).into(ivSubModuleSecondCrown)
-
-                if(thirdSubModule.isDifficultiesCrowned.hard)
-                    Glide.with(itemView!!.context).load(R.drawable.ic_crowned).into(ivSubModuleThirdCrown)
-                else
-                    Glide.with(itemView!!.context).load(R.drawable.ic_uncrowned).into(ivSubModuleThirdCrown)
+                buildSubModuleProgressBar(item, 2)
             }
         }
     }
 
     override val diffUtilBuilder: (List<LearningJourney>, List<LearningJourney>) -> DiffUtil.Callback = { old, new ->
         LearningJourneyDiffUtil(old, new)
+    }
+
+    private fun IncludeSubModuleProgressBarBinding.buildSubModuleProgressBar(item: LearningJourney, position: Int) {
+        this.apply {
+            val subModuleByPosition = item.gameLearningJourneyResponses[position]
+            if (subModuleByPosition.isGameUnlocked) {
+                callGlide(itemView!!.context, item.moduleIconUnlocked, ivSubModuleIcon)
+            } else {
+                callGlide(itemView!!.context, item.moduleIconLocked, ivSubModuleIcon)
+            }
+            apbSubModule.progress = subModuleByPosition.gamePercentage.toInt()
+            crowningSubModule { subModuleByPosition.isDifficultiesCrowned.easy }
+            crowningSubModule { subModuleByPosition.isDifficultiesCrowned.medium }
+            crowningSubModule { subModuleByPosition.isDifficultiesCrowned.hard }
+        }
+    }
+
+    private fun IncludeSubModuleProgressBarBinding.crowningSubModule(condition: () -> Boolean) {
+        if(condition())
+            callGlide(itemView!!.context, R.drawable.ic_crowned, ivSubModuleFirstCrown)
+        else
+            callGlide(itemView!!.context, R.drawable.ic_uncrowned, ivSubModuleFirstCrown)
     }
 }
