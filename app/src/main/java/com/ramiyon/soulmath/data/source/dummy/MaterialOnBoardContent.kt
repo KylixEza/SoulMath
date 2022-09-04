937d5125564e890e484bf8e8b83c112b29804ec9
ACER <loilyxtra@gmail.com>
2022-09-04 22:28:35 +0700

    update: added dummy materialId

--- app/src/main/java/com/ramiyon/soulmath/data/source/dummy/MaterialOnBoardContent.kt
+++ app/src/main/java/com/ramiyon/soulmath/data/source/dummy/MaterialOnBoardContent.kt
@@ -5,9 +5,9 @@ import com.ramiyon.soulmath.domain.model.material.MaterialOnBoard
 
 object MaterialOnBoardContent {
     private fun getMaterialOnBoardContent() = listOf(
-        MaterialOnBoard(materialId = "", page = 1, gif = 0, description = "Tahukah kamu materi apa yang akan kita pelajari kali ini?"),
-        MaterialOnBoard(materialId = "", page = 2, gif = 0, description = "Hari ini, kita akan belajar mengenal angka mulai dari 1-10. Yuk, simak lebih lanjut materinya"),
-        MaterialOnBoard(materialId = "", page = 3, upperImage = 0, lowerImage = 0, description = "Di atas ini adalah contoh dari angka yang akan kita pelajari hari ini!"),
+        MaterialOnBoard(materialId = "MATERIAL001", page = 1, gif = 0, description = "Tahukah kamu materi apa yang akan kita pelajari kali ini?"),
+        MaterialOnBoard(materialId = "MATERIAL001", page = 2, gif = 0, description = "Hari ini, kita akan belajar mengenal angka mulai dari 1-10. Yuk, simak lebih lanjut materinya"),
+        MaterialOnBoard(materialId = "MATERIAL001", page = 3, upperImage = 0, lowerImage = 0, description = "Di atas ini adalah contoh dari angka yang akan kita pelajari hari ini!"),
     )
 
     fun getMaterialOnBoardContentById(materialId: String, page: Int) =
